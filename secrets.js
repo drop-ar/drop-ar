90dc12f6ebd787c4e94195386ed7f65e90b725dc
Jungsoo Bin <witberrybean@gmail.com>
2018-08-17 17:46:09 -0400

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,18 @@
+//FaceBook 
+
+process.env.FACEBOOK_APP_ID = "2112247582376014"
+
+
+process.env.FACEBOOK_CLIENT_SECRET = "dd94f75f3b5e6121e45a245846675be2"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6royGnNb6iwK4qGFZyy6TzEszpLHXkEE",
+  authDomain: "graftarfinal.firebaseapp.com",
+  databaseURL: "https://graftarfinal.firebaseio.com",
+  projectId: "graftarfinal",
+  storageBucket: "graftarfinal.appspot.com",
+  messagingSenderId: "599185074055"
+};
+
+export default firebaseConfig
